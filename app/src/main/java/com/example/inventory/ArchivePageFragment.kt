a39dc67c067e5c4b94a273c942a344fb1e8af680
lux <ssbekmuratov@gmail.com>
2023-04-26 00:21:25 +0600

    bottom navigation

--- app/src/main/java/com/example/inventory/ArchivePageFragment.kt
+++ app/src/main/java/com/example/inventory/ArchivePageFragment.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.inventory.databinding.FragmentArchivePageBinding
 
 class ArchivePageFragment : Fragment() {
+
+    lateinit var binding: FragmentArchivePageBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_archive_page, container, false)
+        binding = FragmentArchivePageBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/inventory/MainActivity.kt
+++ app/src/main/java/com/example/inventory/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.inventory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.inventory.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.bottomNavBar.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.mainMenu -> setFragment(MainPageFragment())
+                R.id.archiveMenu -> setFragment(ArchivePageFragment())
+                else -> setFragment(ArchivePageFragment())
+            }
+        }
     }
+
+    private fun setFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentHolder, fragment)
+            commit()
+        }
+    }
+
 }
